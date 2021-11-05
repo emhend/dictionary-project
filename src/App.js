eaddb1e5144a7760839a12be662e9c3281a96c96
emhend <86583654+emhend@users.noreply.github.com>
2021-11-04 21:09:28 -0500

    added search + body

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
       <h1>Dictionary</h1>
+      <Dictionary />
+      <div className="Container">
+        lalalalalalalalalalalalalalalalalalalalalalalalalalala
+        lalalalalalalalala lalalalalalalalala lalalalalalalalala
+        lalalalalalalalala lalalalalalalalala lalalalalalalalala
+        lalalalalalalalala lalalalalalalalala lalalalalalalalala
+        lalalalalalalalala lalalalalalalalala lalalalalalalalala
+        lalalalalalalalala lalalalalalalalala lalalalalalalalala
+      </div>
     </div>
   );
 }
