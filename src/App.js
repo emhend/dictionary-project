2e84e9458b2bd62877c303bece8910570c08e0a3
emhend <86583654+emhend@users.noreply.github.com>
2021-11-04 20:05:08 -0500

    fixes for deploy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Emily's Dictionary App</h1>
+      <h1>Dictionary</h1>
     </div>
   );
 }
